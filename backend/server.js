5929c154ea82203e20570c6be8fed97a50ddf465
pranjalshukla23 <62809277+pranjalshukla23@users.noreply.github.com>
2022-12-05 01:31:47 +0530

    added cors middleware

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,20 @@ const port = 5000;
 
 connectToMongo();
 
+app.set("trust proxy", 1);
+
 app.use(cors());
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", true);
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE,OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin,X-Requested-With,Content-Type,Accept,content-type,application/json"
+  );
+  next();
+});
 
 app.use(express.json());
 
